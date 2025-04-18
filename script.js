a6d4cc847d82bf4ea79302595473be82a42564b2
Renan Pires Andrade <nanzim@MacBook-Pro-de-Renan.local>
2025-04-18 16:59:09 -0300

    base completa

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const modo = document.getElementById('tema')
+const botaoTema = document.getElementById('modo-escuro');
+const icone = botaoTema.querySelector('i');
 
-modo.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+
+botaoTema.addEventListener('click', () => {
+  // Aplica/remover a classe no <body>
+  document.body.classList.toggle('dark');
+
+  // Alternar ícone
+  if (document.body.classList.contains('dark')) {
+    icone.classList.remove('fa-moon');
+    icone.classList.add('fa-sun');
+  } else {
+    icone.classList.remove('fa-sun');
+    icone.classList.add('fa-moon');
+  }
+});
+
+
+const senhaInput = document.getElementById('senha');
+const togglePassword = document.getElementById('togglePassword');
+const icon = document.getElementById('iconolho');
+
+togglePassword.addEventListener('click', () => {
+  const isPassword = senhaInput.type === 'password';
+  senhaInput.type = isPassword ? 'text' : 'password';
+
+  // Alternar o ícone (olho aberto e fechado)
+  icon.classList.toggle('fa-eye');
+  icon.classList.toggle('fa-eye-slash');
+});
